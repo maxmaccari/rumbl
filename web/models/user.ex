201fe34465b69033b3bbac84eaf64d18e436ba5c
Maxsuel Maccari <maxmaccari@hotmail.com>
2018-01-24 00:25:24 -0300

    Add new user creation

--- web/models/user.ex
+++ web/models/user.ex
@@ -9,4 +9,11 @@ defmodule Rumbl.User do
 
     timestamps
   end
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, ~w(name username)a, [])
+    |> validate_required(~w(name username)a)
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
