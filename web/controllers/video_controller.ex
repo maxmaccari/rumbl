01cdbeb9d46c017b237a17c9c20c979d8494e3dc
Maxsuel Maccari <maxmaccari@hotmail.com>
2018-02-01 05:57:52 -0300

    Add Video Controller integration tests

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -23,10 +23,10 @@ defmodule Rumbl.VideoController do
       |> Video.changeset(video_params)
 
     case Repo.insert(changeset) do
-      {:ok, video} ->
+      {:ok, _} ->
         conn
         |> put_flash(:info, "Video created successfully.")
-        |> redirect(to: video_path(conn, :show, video))
+        |> redirect(to: video_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
